8845d74fc0ef72711aaccc2c446a76db5e4e3674
Jaival Saija <saijajaival@gmail.com>
2021-02-04 11:42:05 +0530

    404 Page Added

---
+++ lib/Screens/Error.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ContactUs extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          "404 Not Found",
+          style: Theme.of(context)
+              .textTheme
+              .headline5
+              .copyWith(color: Colors.black),
+        ),
+      ),
+    );
+  }
+}
